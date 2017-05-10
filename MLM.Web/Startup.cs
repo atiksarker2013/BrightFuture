a63fe3a92867562e99604994b1b02db848817df2
atiksarker2013 <atiour.islam@gmail.com>
2017-05-10 10:59:57 +0600

    Add project files.

---
+++ MLM.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MLM.Web.Startup))]
+namespace MLM.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
