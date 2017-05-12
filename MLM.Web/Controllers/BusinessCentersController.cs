d90e7bc390336b5b46b86453ec554d7e70cd15ae
atiksarker2013 <atiour.islam@gmail.com>
2017-05-12 23:57:24 +0600

    ff

---
+++ MLM.Web/Controllers/BusinessCentersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MLM.Web.Controllers
+{
+    public class BusinessCentersController : Controller
+    {
+        // GET: BusinessCenters
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
